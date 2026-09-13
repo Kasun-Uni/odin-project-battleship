38febbe563d175879201cf71ac5afa55890c2e3d
Kasun <kasun.kanakarathne@unicast.ne.jp>
2026-09-13 11:19:36 +0900

    feat: feat: npm, Jest, Babel, Webpackのセットアップ

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+import path from "node:path";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+
+export default {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(import.meta.dirname, "dist"),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./template.html",
+    }),
+  ],
+  devServer: {
+    port: 8080,
+    watchFiles: ["./template.html"],
+  },
+};
